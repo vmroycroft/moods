a707bb887acc621aa3d03b942286640aeb9cdca3
Vanessa Roycroft <vmroycroft@gmail.com>
2021-07-11 11:11:41 -0400

    Updated heatmap colors

--- pages/MoodCalendar.js
+++ pages/MoodCalendar.js
@@ -1,9 +1,20 @@
 import { ResponsiveCalendar } from '@nivo/calendar';
 import { format, parseISO } from 'date-fns';
+import { sample } from 'lodash';
 import useSWR from 'swr';
 import axios from 'axios';
 
 function MoodCalendar() {
+	// Tailwind colors 900, 700, 500, 300
+	const colors = {
+		orange: ['#7C2D12', '#C2410C', '#F97316', '#FDBA74'],
+		lime: ['#365314', '#4D7C0F', '#84CC16', '#BEF264'],
+		emerald: ['#064E3B', '#047857', '#10B981', '#6EE7B7'],
+		sky: ['#0C4A6E', '#0369A1', '#0EA5E9', '#7DD3FC'],
+		purple: ['#581C87', '#7E22CE', '#A855F7', '#D8B4FE'],
+		pink: ['#831843', '#BE185D', '#EC4899', '#F9A8D4']
+	};
+
 	const MOOD_VALUES = {
 		Great: 1,
 		Meh: 2,
@@ -40,7 +51,7 @@ function MoodCalendar() {
 						from="2021-03-01"
 						to="2021-07-12"
 						emptyColor="#eeeeee"
-						colors={['#61cdbb', '#97e3d5', '#e8c1a0', '#f47560']}
+						colors={sample(colors)}
 						margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
 						yearSpacing={40}
 						monthBorderColor="#ffffff"
